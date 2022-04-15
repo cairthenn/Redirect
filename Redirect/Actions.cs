41cb6eecd63cc43c7ed7e5cd1b4633127bc30def
Cair <mgordon52192@gmail.com>
2022-04-15 15:48:20 -0400

    Fix/improve action filtering
    Fixes #13

--- Redirect/Actions.cs
+++ Redirect/Actions.cs
@@ -57,13 +57,7 @@ namespace Redirect {
             foreach (var job in JobInfo) {
                 Jobs[job] = Sheet.Where(a => {
 
-                    if (a.IsActionAllowed()) {
-                        return false;
-                    }
-
-                    var cj = a.ClassJob.Value;
-
-                    if (!a.IsPlayerAction || a.IsRoleAction) {
+                    if (a.IsActionBlocked() || a.ClassJob.Row + 1 == 0 || !a.IsPlayerAction || a.IsRoleAction) {
                         return false;
                     }
 
